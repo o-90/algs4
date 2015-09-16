98b12b682dcbd545f616478cf6b5e08eafa16a00
GoBrewers14 <john.r.martinez14@gmail.com>
2015-09-16 15:04:19 -0500

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# algs4
-### code from Princeton's Algorithms on Coursera.
---
+++ quick_find.py
@@ -0,0 +1,37 @@
+class QuickFindUF(object):
+    '''
+    union find
+    '''
+    def __init__(self, N):
+        self.N = N
+        self.idx = range(N)
+        self.count = 0
+
+    def connected(self, p, q):
+        '''
+        check if p & q share a connection.
+        '''
+        if self.idx[p] == self.idx[q]:
+            return True
+        else:
+            return False
+
+    def union(self, p, q):
+        '''
+        union p & q together
+        ''' 
+        p_id = self.idx[p]
+        q_id = self.idx[q]
+
+        for i in xrange(self.N):
+            if self.idx[i] == p_id:
+                self.idx[i] = q_id
+
+# test client
+if __name__ == "__main__":
+    uf = QuickFindUF(100)
+    uf.union(6, 5)
+    uf.union(2, 3)
+    uf.union(1, 4)
+    print "Are {0} and {1} connected?\n{2}".format(1, 4, uf.connected(1, 4))
+    print "Are {0} and {1} connected?\n{2}".format(1, 5, uf.connected(1, 5))
