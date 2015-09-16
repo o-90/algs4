24a89c04a8a62cb23e9b8268db3d8a25494e7867
John Martinez <john.r.martinez14@gmail.com>
2015-09-15 20:44:01 -0500

    updated ; inplemented isOpen(), isFull() and open() in Percolation.java

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -4,34 +4,70 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-  private boolean[][] grid;
+  private int N=0;
+  private boolean[] grid = null;
+  private WeightedQuickUnionUF uf = null;
 
   public Percolation(int N) {
-    grid = new boolean[N][N];
+    grid = new boolean[N*N+2];
     for (int i=0; i<N; i++)
       for (int j=0; j<N; j++)
-        grid[i][j] = false;
+        grid[i*N+j] = false;
+
+    // make dummy connections for top and bottom row
+    grid[N * N + 1] = true;
+    grid[N * N + 2] = true;
+
+    uf = new WeightedQuickUnionUF(N*N+2);
   }
 
-  //public void open(int i, int j)          // open site if its not open
-  //public boolean isOpen(int i, int j)     // is site open?
-  //public boolean isFull(int i, int j)     // is site full?
-  //public boolean percolates()             // does system percolate?
-  //                                        //
-  //public static void main(String[] args)  // test client
-
-  public void open(int i, int j)
-  {  grid[i][j]=true;  }
+  public void open(int i, int j) {
+    if (i < 1 || j < 1 || i > N || j > N)
+      throw new java.lang.IndexOutOfBoundsException();
+    int a = i - 1;
+    int b = j - 1;
+    if (!grid[a * N + b]) {
+      grid[a * N + b] = true;
+      // up
+      if((a-1) > 0 && b > 0 && (a-1) < N && b < N && grid[(a-1)*N+b])
+        uf.union(a*N+b, (a-1)*N+b);
+      // down
+      if((a+1) > 0 && b > 0 && (a+1) < N && b < N && grid[(a+1)*N+b])
+        uf.union(a*N+b, (a+1)*N+b);
+      // left
+      if(a > 0 && (b-1) > 0 && a < N && (b-1) < N && grid[a*N+(b-1)])
+        uf.union(a*N+b, a*N+(b-1));
+      // right
+      if(a > 0 && (b+1) > 0 && a < N && (b+1) < N && grid[a*N+(b+1)])
+        uf.union(a*N+b, a*N+(b+1));
+      if (i == 1)
+        uf.union(a * N + b, N * N);
+      if (i == N)
+        uf.union(a * N + b, N * N + 1);
+    }
+  }
 
   public boolean isOpen(int i, int j) {
-    if (grid[i][j] != false)
-      return true;
-    return false;
+    // is i,j open?
+    int a = i - 1;
+    int b = j - 1;
+    if (grid[a * N + b] == false) return true;
+    else                          return false;
   }
 
-  //public boolean isFull(int i, int j) {
-  //}
+  public boolean isFull(int i, int j) {
+    // is i,j connected to top node?
+    int a = i - 1;
+    int b = j - 1;
+    return uf.connected(N * N + 1, a * N + b);
+  }
 
-  //public boolean percolates() {
-  //}
+  public boolean percolates() {
+    return uf.connected(N * N, N * N + 1);
+  }
+
+  public static void main(String[] args) {
+    Percolation p = new Percolation(4);
+    StdOut.println(p + " ");
+  }
 }
