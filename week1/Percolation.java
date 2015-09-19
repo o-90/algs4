95a6828e1038637ac85e40a7afe5cd4b8be00705
John Martinez <john.r.martinez14@gmail.com>
2015-09-19 11:31:36 -0500

    final percolation

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -5,108 +5,105 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-  private int N = 0;
-  private boolean[] grid = null;
-  private WeightedQuickUnionUF uf = null;
+	private int N = 0;
+	private boolean[] grid = null;
+	private WeightedQuickUnionUF uf = null;
 
-  public Percolation(int N) {
+	public Percolation(int N) {
 
-    // empty grid
-    grid = new boolean[N * N + 2];
-    for (int i=0; i<N; i++)
-      for (int j=0; j<N; j++)
-        grid[i * N + j] = false;
+		// initialize empty grid
+		grid = new boolean[N * N + 2];
+    	for (int i = 0; i < N; i++)
+    		for (int j = 0; j < N; j++)
+    			grid[i * N + j] = false;
 
-    // make dummy connections for top and bottom
-    grid[N * N]     = true;
-    grid[N * N + 1] = true;
+    	// make dummy connections for top and bottom
+    	grid[N * N]     = true;
+    	grid[N * N + 1] = true;
 
-    // create quick union network
-    this.N = N;
-    uf = new WeightedQuickUnionUF(N * N + 2);
-  }
-
-  public void open(int i, int j) {
-    // create open sites
-
-    if (i < 1 || j < 1 || i > N || j > N)
-      throw new java.lang.IndexOutOfBoundsException();
-    int a = i - 1;
-    int b = j - 1;
-
-    if (!grid[a * N + b]) {
-      grid[a * N + b] = true;
-
-      // up
-      if((a-1) >= 0 && b >= 0 && (a-1) < N && b < N && grid[(a-1)*N+b])
-        uf.union(a * N + b, (a - 1) * N + b);
-      // down
-      if((a+1) >= 0 && b >= 0 && (a+1) < N && b < N && grid[(a+1)*N+b])
-        uf.union(a * N + b, (a + 1) * N + b);
-      // left
-      if(a >= 0 && (b-1) >= 0 && a < N && (b-1) < N && grid[a*N+(b-1)])
-        uf.union(a * N + b, a * N + (b - 1));
-      // right
-      if(a >= 0 && (b+1) >= 0 && a < N && (b+1) < N && grid[a*N+(b+1)])
-        uf.union(a * N + b, a * N + (b + 1));
-      // top
-      if (i == 1)
-        uf.union(a * N + b, N * N);
-      // bottom
-      if (i == N)
-        uf.union(a * N + b, N * N + 1);
+    	// create quick union network
+    	this.N = N;
+    	uf = new WeightedQuickUnionUF(N * N + 2);
     }
-  }
+    public void open(int i, int j) {
+    	// create open sites
+    	if (i < 1 || j < 1 || i > N || j > N)
+    		throw new java.lang.IndexOutOfBoundsException();
+    	int a = i - 1;
+    	int b = j - 1;
 
-  public boolean isOpen(int i, int j) {
-    // is i,j open?
-    int a = i - 1;
-    int b = j - 1;
-    return grid[a * N + b];
-  }
+    	if (!grid[a * N + b]) {
+    		grid[a * N + b] = true;
 
-  public boolean isFull(int i, int j) {
-    // is i,j connected to top node?
-    int a = i - 1;
-    int b = j - 1;
-    return uf.connected(N * N, a * N + b);
-  }
-
-  public boolean percolates()
-  {  return uf.connected(N * N, N * N + 1);  }
-
-  // test client
-  public static void main(String[] args) {
-
-    int [] count = new int[20];
-    double r = 0.61;
-    int T = 20;
-    int n = 5;
-    double mean = 0.0;
-    double stddev = 0.0;
-
-    for (int t=0; t<T; t++) {
-
-      Percolation p = new Percolation(n);
-      for (int i=1; i<=n; i++) {
-        for (int j=1; j<=n; j++) {
-          if (!StdRandom.bernoulli(r)) {
-            p.open(i, j);
-          }
-        }
-      }
-      if (p.percolates()) count[t] = 1;
-      else                count[t] = 0;
+    		// up
+    		if((a-1) >= 0 && b >= 0 && (a-1) < N && b < N && grid[(a-1)*N+b])
+    			uf.union(a * N + b, (a - 1) * N + b);
+    		// down
+    		if((a+1) >= 0 && b >= 0 && (a+1) < N && b < N && grid[(a+1)*N+b])
+    			uf.union(a * N + b, (a + 1) * N + b);
+    		// left
+    		if(a >= 0 && (b-1) >= 0 && a < N && (b-1) < N && grid[a*N+(b-1)])
+    			uf.union(a * N + b, a * N + (b - 1));
+    		// right
+    		if(a >= 0 && (b+1) >= 0 && a < N && (b+1) < N && grid[a*N+(b+1)])
+    			uf.union(a * N + b, a * N + (b + 1));
+    		// top
+    		if (i == 1)
+    			uf.union(a * N + b, N * N);
+    		// bottom
+    		if (i == N)
+    			uf.union(a * N + b, N * N + 1);
+    	}
     }
-    for (int i=0; i<20; i++)
-      mean += count[i];
 
-    mean /= T;
-    for (int i=0; i<20; i++)
-      stddev += (count[i] - mean) * (count[i] - mean);
+    public boolean isOpen(int i, int j) {
+    	// is i,j open?
+    	int a = i - 1;
+    	int b = j - 1;
+    	return grid[a * N + b];
+    }
 
-    stddev /= (T - 1);
-    StdOut.println("percentage that percolates\t\t" + mean);
-    StdOut.println("standard deviation\t\t" + stddev);
-  }
+    public boolean isFull(int i, int j) {
+    	// is i,j connected to top node?
+    	int a = i - 1;
+    	int b = j - 1;
+    	return uf.connected(N * N, a * N + b);
+    }
+
+    public boolean percolates()
+    {  return uf.connected(N * N, N * N + 1);  }
+
+    // test client
+    public static void main(String[] args) {
+
+    	int [] count = new int[20];
+	    double r = 0.61;
+	    int T = 20;
+	    int n = 5;
+	    double mean = 0.0;
+	    double stddev = 0.0;
+
+	    for (int t = 0; t < T; t++) {
+	    	Percolation p = new Percolation(n);
+	    	for (int i = 1; i <= n; i++) {
+	    		for (int j = 1; j <= n; j++) {
+	    			if (!StdRandom.bernoulli(r)) {
+	    				p.open(i, j);
+	    			}
+	    		}
+	    	}
+	    	if (p.percolates()) count[t] = 1;
+	    	else                count[t] = 0;
+	    }
+	    for (int i = 0; i < T; i++)
+	    	mean += count[i];
+
+	    mean /= T;
+	    for (int i = 0; i < T; i++)
+	    	stddev += (count[i] - mean) * (count[i] - mean);
+
+	    stddev /= (T - 1);
+	    StdOut.println("percentage that percolates\t\t" + mean);
+	    StdOut.println("standard deviation\t\t" + stddev);
+	}
 }
--- week1/PercolationStats.java
+++ week1/PercolationStats.java
@@ -4,64 +4,59 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-  private double mean;
-  private double stddev;
-  private double upper;
-  private double lower;
+	private double mean;
+	private double stddev;
+	private double upper;
+	private double lower;
 
-  public PercolationStats(int N, int T) {
-    if (N <= 0 || T <= 0)
-      throw new java.lang.IllegalArgumentException();
+	public PercolationStats(int N, int T) {
+		if (N <= 0 || T <= 0)
+			throw new java.lang.IllegalArgumentException();
 
-    double[] count = new double[T];
+		double[] counter = new double[T];
+		for (int t = 0; t < T; t++) {
 
-    for (int t=0; t<T; t++) {
+			int count = 0;
+			Percolation p = new Percolation(N);
 
-      int counter = 0;
-      Percolation p = new Percolation(N);
+			do {
+				int a = StdRandom.uniform(1, N+1);
+				int b = StdRandom.uniform(1, N+1);
+				if (!p.isOpen(a, b)) {
+					p.open(a, b);
+					count++;
+				}
+			} while (!p.percolates());
+			counter[t] = ((double)count / (double)(N * N));
+		}
+		// mean
+    	mean = StdStats.mean(counter);
 
-      while(!p.percolates()) {
-        int a = StdRandom.uniform(1,N);
-        int b = StdRandom.uniform(1,N);
-        if (!p.isOpen(a, b)) {
-          counter++;
-          p.open(a, b);
-        }
-      }
-      count[t] = counter;
-    }
-    // mean
-    //for (int t=0; t<T; t++) mean += count[t];
-    //mean /= T;
-    mean = StdStats.mean(count);
+	    // standard deviation
+	    stddev = StdStats.stddev(counter);
 
-    // standard deviation
-    //for (int t=0; t<T; t++) stddev += (count[t]-mean)*(count[t] - mean);
-    //stddev /= (T-1);
-    //stddev = Math.sqrt(stddev);
-    stddev = StdStats.stddev(count);
+	    // confidence intervals
+	    lower = mean - ((1.96 * stddev) / Math.sqrt(T));
+	    upper = mean + ((1.96 * stddev) / Math.sqrt(T));
+	}
+	public double mean()
+	{  return mean;  }
 
-    // confidence intervals
-    lower = mean - ((1.96 * stddev) / Math.sqrt(T));
-    upper = mean + ((1.96 * stddev) / Math.sqrt(T));
-  }
+	public double stddev()
+	{  return stddev;  }
 
-  public double mean()
-  {  return mean;  }
+	public double confidenceLo()
+	{  return lower;  }
 
-  public double stddev()
-  {  return stddev;  }
+	public double confidenceHi()
+	{  return upper;  }
 
-  public double confidenceLo()
-  {  return lower;  }
-
-  public double confidenceHi()
-  {  return upper;  }
-
-  public static void main(String[] args) {
-    PercolationStats ps = new PercolationStats(20, 10);
-    StdOut.println("mean\t\t\t = " + ps.mean());
-    StdOut.println("stddev\t\t = " + ps.stddev());
-    StdOut.println("95% conf\t = " + ps.confidenceLo() + ", " + ps.confidenceHi());
-  }
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		int t = Integer.parseInt(args[1]);
+		PercolationStats ps = new PercolationStats(n, t);
+	    StdOut.println("mean          = " + ps.mean());
+	    StdOut.println("stddev        = " + ps.stddev());
+	    StdOut.println("95% conf      = " + ps.confidenceLo() + ", " + ps.confidenceHi());
+	}
 }
