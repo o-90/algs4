e9bc4b5857f5351ea881779084e12292abe8b434
John Martinez <john.r.martinez14@gmail.com>
2015-09-20 18:07:50 -0500

    indention is cray cray

--- week2/Deque.java
+++ week2/Deque.java
@@ -1,55 +1,52 @@
 import java.util.Iterator;
-
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-
 public class Deque<Item> implements Iterable<Item> {
+  private Node first;
+  private Node last;
+  private int  N;
 
-	private Node first;
-	private Node last;
-	private int  N;
+  private class Node {
+  	Item item;
+  	Node next;
+  	Node prev;
+  }
 
-	private class Node {
-        Item item;
-	    Node next;
-	    Node prev;
-	}
+  public Deque() {
 
-	public Deque() {
+  }
 
-	}
+  public boolean isEmpty() {
+  	return true;
+  }
 
-	public boolean isEmpty() {
-		return true;
-	}
+  public int size() {
+  	return 1;
+  }
 
-	public int size() {
-		return 1;
-	}
+  public void addFist(Item item) {
 
-	public void addFist(Item item) {
+  }
 
-	}
+  public void addLast(Item item) {
 
-	public void addLast(Item item) {
+  }
 
-	}
+  public Item removeFirst() {
+  //return item;
+  }
 
-	public Item removeFirst() {
-	//return item;
-	}
+  public Item removeLast() {
+  //return item;
+  }
 
-	public Item removeLast() {
-	//return item;
-	}
+  public Iterator<Item> iterator() {
+  // iterator()	
+  }
 
-	public Iterator<Item> iterator() {
+  public static void main(String[] args) {
 
-	}
-
-	public static void main(String[] args) {
-
-	}
+  }
 }
