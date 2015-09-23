3a2cb137a82a23b68524a843340202bbc4300d7f
John Martinez <john.r.martinez14@gmail.com>
2015-09-23 17:11:13 -0500

    randomized queue init

---
+++ week2/RandomizedQueue.java
@@ -0,0 +1,12 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterator<Item> {
+  public RandomizedQueue<Item>()
+  public boolean isEmpty()
+  public int size()
+  public void enqueue(Item item)
+  public Item dequeue()
+  public Item sample()
+  public Iterator<Item> iterator()
+  public static void main(String[] args)
+}
