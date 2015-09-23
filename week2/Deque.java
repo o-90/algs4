8a14c17d1fec4f283507011a02e1541f3bf15f4b
John Martinez <john.r.martinez14@gmail.com>
2015-09-22 20:54:35 -0500

    deque initialization

--- week2/Deque.java
+++ week2/Deque.java
@@ -4,48 +4,111 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class Deque<Item> implements Iterable<Item> {
-	private Node first;
-	private Node last;
+	private Node<Item> first;
+	private Node<Item> last;
 	private int  N;
 
-	private class Node {
+	private class Node<Item> {
 		Item item;
-		Node next;
-		Node prev;
+		Node<Item> next;
+		Node<Item> prev;
 	}
 
 	public Deque() {
-
+		first = null;
+		last  = null;
+		N     = 0;
 	}
 
 	public boolean isEmpty() {
-		return true;
+		return N == 0;
 	}
 
 	public int size() {
-		return 1;
+		return N;
 	}
 
-	public void addFist(Item item) {
+	public void addFirst(Item item) {
+		if (item == null)
+			throw new java.lang.NullPointerException();
+		Node<Item> newFirst = new Node<Item>();
+		newFirst.item = item;
+		newFirst.prev = null;
+		newFirst.next = first;
 
+		if (isEmpty())
+		{  last = newFirst;  }
+		else
+	    {  first.prev = newFirst;  }
+		first = newFirst;
+		N++;
 	}
+
 	public void addLast(Item item) {
+		if (item == null)
+			throw new java.lang.NullPointerException();
+		Node<Item> newLast = new Node<Item>();
+		newLast.item = item;
+		newLast.prev = last;
+		newLast.next = null;
 
+		if (isEmpty())
+		{  first = newLast;  }
+		else
+		{  last.next = newLast; }
+		last = newLast;
+		N++;
 	}
 
 	public Item removeFirst() {
-	//return item;
+		//return item;
+		if (N == 0)
+			throw new java.util.NoSuchElementException();
+		Item firstElement = first.item;
+		first = first.next;
+		N--;
+
+		if (isEmpty())
+		{  last = null;  }
+		else
+		{  first.prev = null;  }
+
+		return firstElement;
 	}
 
 	public Item removeLast() {
-	//return item;
+		//return item;
+		if (N == 0)
+			throw new java.util.NoSuchElementException();
+		Item lastElement = last.item;
+		last = last.prev;
+		N--;
+
+		if (isEmpty())
+		{  first = null;  } 
+		else
+		{  last.next = null;  }
+
+		return lastElement;
 	}
 
 	public Iterator<Item> iterator() {
-	// iterator()	
+		// iterator()	
+		return new ListIterator();
 	}
 
-	public static void main(String[] args) {
-
+	private class ListIterator implements Iterator<Item> {
+		private Node<Item> current = first;
+		public boolean hasNext()
+		{  return current != null;  }
+		public void remove() { }
+		public Item next() {
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
 	}
+	
+	//public static void main(String[] args) {
+	//}
 }
