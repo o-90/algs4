433689f1c125f237b3995763e41a3d579a06cc70
John Martinez <john.r.martinez14@gmail.com>
2015-09-23 17:18:03 -0500

    added ListIterator<Item>

--- week2/RandomizedQueue.java
+++ week2/RandomizedQueue.java
@@ -1,12 +1,54 @@
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterator<Item> {
-  public RandomizedQueue<Item>()
+  private Node<Item> first;
+  private Node<Item> last;
+  private N;
+
+  private class Node<Item> {
+  	Item item;
+  	Node next;
+  }
+
+  public RandomizedQueue<Item>() {
+  	first = null;
+  	last = null;
+  	N = 0;
+  }
+
   public boolean isEmpty()
+  {  return N == 0}
+
   public int size()
-  public void enqueue(Item item)
-  public Item dequeue()
-  public Item sample()
+  {  return N;  }
+
+  public void enqueue(Item item) {
+
+  }
+
+  public Item dequeue() {
+
+  }
+
+  public Item sample() {
+
+  }
+
   public Iterator<Item> iterator()
-  public static void main(String[] args)
+  {  return new ListIterator();  }
+
+  private class ListIterator implements Iterator<Item> {
+  	private Node<Item> current = first;
+  	public boolean hasNext();
+  	{  return current = null;  }
+  	public void remove()
+    {  /* put exception handling here */  }
+    public Item next() {
+    	Item item = current.item;
+    	current = current.next;
+    	return item;
+    }
+  }
+  public static void main(String[] args) {
+  }
 }
